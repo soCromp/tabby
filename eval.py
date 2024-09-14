7fa3426b3a145b74ab783afa844a93b673985f15
soCromp <snc40@pitt.edu>
2024-09-14 14:15:37 -0500

    1 path in eval arg

--- eval.py
+++ eval.py
@@ -4,20 +4,31 @@ import sys
 import json 
 from sklearn import preprocessing, pipeline, ensemble, compose
 from sklearn.metrics import *
+import os 
 
-configpath = sys.argv[-1]
-with open(configpath) as f:
-    config = json.load(f)
-    
-with open(f'./data/{config["dataset"]}/latest/config.json') as f: 
+ckptpath = sys.argv[-1]
+synth = pd.read_csv(os.path.join(ckptpath, 'samplesclean.csv'))
+with open(os.path.join(ckptpath, 'dataconfig.json')) as f:
     dataconfig = json.load(f)
-    
-d = {}
-d['real'] = pd.read_csv(f'./data/{config["dataset"]}/latest/test.csv')
-config.pop('dataset')
 
-for name, paths in config.items():
-    d[name] = pd.read_csv(paths[0])
+trainpath = f'./data/{dataconfig["dataset_name"]}/latest/train.csv'
+testpath = f'./data/{dataconfig["dataset_name"]}/latest/test.csv'
+train = pd.read_csv(trainpath)
+test = pd.read_csv(testpath)
+
+# configpath = sys.argv[-1]
+# with open(configpath) as f:
+#     config = json.load(f)
+    
+# with open(f'./data/{config["dataset"]}/latest/config.json') as f: 
+#     dataconfig = json.load(f)
+    
+# d = {}
+# d['real'] = pd.read_csv(f'./data/{config["dataset"]}/latest/test.csv')
+# config.pop('dataset')
+
+# for name, paths in config.items():
+#     d[name] = pd.read_csv(paths[0])
 
 
 nums = dataconfig['nums']
@@ -32,15 +43,17 @@ def to_float_or_nan(value):
         return np.nan
     
 if dataconfig['task'] == 'classification':
-    labvals = set([l.strip() for l in d['real'][labs[0]].unique()])
+    labvals = set([l.strip() for l in train[labs[0]].unique()]) | \
+                set([l.strip() for l in test[labs[0]].unique()])
+    # print(labvals)
     
-for (k, df) in d.items():
+def preprocess_df(df, categoriesdict):
     # remove extra spaces around strings, eg ' dog' -> 'dog'
     df = df.map(lambda x: x.strip() if type(x) == str else x)
+    df.loc[:,nums] = df.loc[:,nums].map(to_float_or_nan)
+    df.loc[:,ords] = df.loc[:,ords].fillna('?')
     for col in ords:
         categoriesdict[col] = categoriesdict.get(col, []) + df[col].unique().tolist()
-    
-    df.loc[:,nums] = df.loc[:,nums].map(to_float_or_nan)
     
     if dataconfig['task'] == 'classification':
         df = df[df[labs[0]].isin(labvals)]
@@ -48,21 +61,29 @@ for (k, df) in d.items():
         df.loc[:,labs[0]] = df.loc[:,labs[0]].map(to_float_or_nan)
         df = df[~df.isna()[labs[0]]]
         
-    df = df.dropna()
+    # df = df.dropna()
         
     # print(k, df[labs[0]].mean(), df[labs[0]].std())
-    df = df[d['real'].columns]
+    df = df[train.columns] # put all in same order
     # df = df.sample(2000)
     
     for colname in df.columns:
-        df[colname] = df[colname].astype(d['real'][colname].dtype)
+        df[colname] = df[colname].astype(train[colname].dtype)
         
     # print(k, '\t\t', len(df))
-    d[k] = df
+    return df, categoriesdict
+
+train, categoriesdict = preprocess_df(train, categoriesdict)
+test, categoriesdict = preprocess_df(test, categoriesdict)
+synth, categoriesdict = preprocess_df(synth, categoriesdict)
+# print(train)
+# print(test)
+# print(synth)
 
 categories = []
 for col in ords:
     categories.append(list(set(categoriesdict[col])))
+
 ordenc = preprocessing.OrdinalEncoder(categories=categories)
 numenc = preprocessing.StandardScaler()
 
@@ -83,17 +104,29 @@ if dataconfig['task'] == 'classification':
         preprocessed_labels = lb.fit_transform(trainset[labs[0]]).ravel()
         complete_pipeline.fit(trainset[ords+nums], preprocessed_labels)
         return complete_pipeline
-
-    real = d['real']
-    labels = lb.fit_transform(real[labs[0]])
-
+    
+    labels = lb.fit_transform(test[labs[0]])
     results = []
     columns = ['run', 'n', 'acc']
-    for k, df in d.items():
-        rfc = create_classification_pipeline(df)
-        score = rfc.score(real[ords+nums], labels)
-        results.append((k, len(df), score))
-        # print(k, '\t\t', score)
+    
+    rfc_real = create_classification_pipeline(train)
+    score = rfc_real.score(test[ords+nums], labels)
+    results.append(('real', len(train), score))
+    
+    rfc_synth = create_classification_pipeline(synth)
+    score = rfc_synth.score(test[ords+nums], labels)
+    results.append(('synth', len(synth), score))
+
+    # real = d['real']
+    # labels = lb.fit_transform(real[labs[0]])
+
+    # results = []
+    # columns = ['run', 'n', 'acc']
+    # for k, df in d.items():
+    #     rfc = create_classification_pipeline(df)
+    #     score = rfc.score(real[ords+nums], labels)
+    #     results.append((k, len(df), score))
+    #     # print(k, '\t\t', score)
 
 else:
     def create_regression_pipeline(trainset):
@@ -118,7 +151,7 @@ else:
     results = []
     for k, df in d.items():
         rfc = create_regression_pipeline(df)
-        print(df.describe())
+        # print(df.describe())
         # labels = (df[labs[0]]-df[labs[0]].mean()) / df[labs[0]].std()
         rsq = rfc.score(real[ords+nums], labels)
         y_pred = rfc.predict(real[ords+nums])
